1a0b23522cc94bddad8996636c3e2bb4c2f71838
Harshiii3110 <harshitha311006@gmail.com>
2026-06-13 09:28:32 +0530

    Add Binary Strings With Equal Sum Of Two Halves solution

---
+++ Binary_Strings_With_Equal_Sum_Of_Two_Halves.py
@@ -0,0 +1,11 @@
+class Solution:
+    def computeValue(self, n):
+        # code here
+        MOD = 10**9 + 7
+        fact = [1] * (2 * n + 1)
+        for i in range(1, 2 * n + 1):
+            fact[i] = (fact[i - 1] * i) % MOD
+        numerator = fact[2 * n]
+        denominator = (fact[n] * fact[n]) % MOD
+        inverse = pow(denominator, MOD - 2, MOD)
+        return (numerator * inverse) % MOD        
