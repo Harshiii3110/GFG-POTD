bdc898f9060451d57dc21a38244b5ace7df85499
Harshiii3110 <harshitha311006@gmail.com>
2026-06-15 21:32:16 +0530

    Add Minimum Cost To Fill Given Weight solution

---
+++ Minimum_Cost_To_Fill_Given_Weight.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumCost(self, cost, w):
+        # code here
+        INF = float('inf')
+        dp = [INF] * (w + 1)
+        dp[0] = 0
+        n = len(cost)
+        for i in range(n):
+            if cost[i] == -1:
+                continue
+            weight = i + 1
+            for j in range(weight, w + 1):
+                dp[j] = min(dp[j],
+                            dp[j - weight] + cost[i])
+        if dp[w] == INF:
+            return -1
+        return dp[w]        
