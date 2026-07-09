10fe86704981f2f0d0ebb5ca9dad4bec3bbf16db
Harshiii3110 <harshitha311006@gmail.com>
2026-07-09 06:12:21 +0530

    Add Count Pairs Divisible By K solution

---
+++ Count_Pairs_Divisible_By_K.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countKdivPairs(self, arr, k):
+        # code here
+        freq = [0] * k
+        ans = 0
+        for num in arr:
+            rem = num % k
+            need = (k - rem) % k
+            ans += freq[need]
+            freq[rem] += 1
+        return ans        
