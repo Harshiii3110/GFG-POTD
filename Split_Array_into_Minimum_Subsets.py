8aab2ed626ad9a8323444c682c06f1bb5ab80888
Harshiii3110 <harshitha311006@gmail.com>
2026-07-29 18:56:53 +0530

    Add Split Array into Minimum Subsets solution using Sorting

---
+++ Split_Array_into_Minimum_Subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minSubsets(self, arr):
+        #code here
+        arr.sort()
+        count = 1
+        for i in range(1, len(arr)):
+            if arr[i] != arr[i - 1] + 1:
+                count += 1
+        return count        
