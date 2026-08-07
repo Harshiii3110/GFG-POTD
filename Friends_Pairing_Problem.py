93a5797f4a6db4f4f4328ce7563adf44f8c8f844
Harshiii3110 <harshitha311006@gmail.com>
2026-08-07 12:27:14 +0530

    Add Friends Pairing Problem solution using dynamic programming

---
+++ Friends_Pairing_Problem.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countFriendsPairings(self, n: int) -> int:
+        # code here 
+        if n <= 2:
+            return n
+        a = 1
+        b = 2
+        for i in range(3, n + 1):
+            c = b + (i - 1) * a
+            a = b
+            b = c
+        return b    
