68ece431bd800d88eb1aa1d0a3d54712fcb92360
Harshiii3110 <harshitha311006@gmail.com>
2026-09-22 19:19:11 +0530

    Find longest dictionary word by subsequence matching

---
+++ longest_matching_dictionary.py
@@ -0,0 +1,25 @@
+from bisect import bisect_right
+class Solution:
+    def findLongestWord(self, s: str, d: list) -> str:
+        # code here
+        positions = [[] for _ in range(26)]
+        for i, ch in enumerate(s):
+            positions[ord(ch) - ord('a')].append(i)
+        def isSubsequence(word):
+            prev = -1
+            for ch in word:
+                arr = positions[ord(ch) - ord('a')]
+                # Find first position greater than prev
+                idx = bisect_right(arr, prev)
+                if idx == len(arr):
+                    return False
+                prev = arr[idx]
+            return True
+        answer = ""
+        for word in d:
+            if isSubsequence(word):
+                if len(word) > len(answer):
+                    answer = word
+                elif len(word) == len(answer) and word < answer:
+                    answer = word
+        return answer
